e9199f8e64d39b587b49b1570f416490cbd315b4
nuo.xun <nuo.xun@alibaba-inc.com>
2015-04-13 23:24:51 +0800

    fix
    fix

--- book/plugin.js
+++ book/plugin.js
@@ -1,16 +1,29 @@
 require(["gitbook"], function(gitbook) {
 
     var autoSizeIframes = function(){
-    	alert(1);
+        var iframes = document.getElementsByTagName('iframe');
+        for(var i = 0; i < iframes.length; i++) {
+            var iframe = iframes[i],
+                iframeDocument = iframe.contentDocument.documentElement
+                ;
+
+            iframe.setAttribute('scrolling','no');
+            iframe.style.boxSizing = 'content-box';
+
+            iframe.style.height = iframeDocument.scrollHeight + 'px';
+            iframe.style.width = iframeDocument.scrollWidth + 'px';
+        }
     };
 
     gitbook.events.bind("start", function(e, config) {
-    	pluginConfig = config;
-    	autoSizeIframes();
+        pluginConfig = config;
     });
 
     gitbook.events.bind("page.change", function() {
-    	autoSizeIframes();
+        setInterval(function(){
+            autoSizeIframes();
+        },200);
+
     });
 
     gitbook.events.bind("exercise.submit", function(e, data) {
